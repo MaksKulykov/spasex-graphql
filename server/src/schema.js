aff712c19a487ebecdb4c3372083e496a358510d
maksim.k.as@agiliumlabs.com <kulykov.maks@gmail.com>
2022-05-11 10:11:57 +0200

    add data to schema

--- server/src/schema.js
+++ server/src/schema.js
@@ -2,16 +2,26 @@ const { gql } = require('apollo-server');
 
 const typeDefs = gql`
     type Query {
-      "Get missions array for homepage grid"
-      missions: [Mission!]!
-      "Fetch a specific mission, provided a mission's ID"
-      mission(id: ID!): Mission
+        "Get missions array for homepage grid"
+        missions: [Mission!]!
+        "Fetch a specific mission, provided a mission's ID"
+        mission(id: ID!): Mission
     }
     "A mission is a group of Modules that teaches about a specific topic"
     type Mission {
-      id: ID!
-      "The mission's name"
-      name: String!
+        id: ID!
+        "The mission's name"
+        name: String!
+        "The mission's description"
+        description: String
+        "Array of manufactures"
+        manufactured: [String]
+        "Link for mission in twitter"
+        twitter: String
+        "Link for mission's website'"
+        website: String
+        "Link for mission's page in wikipedia'"
+        wikipedia: String
     }
 `;
 
