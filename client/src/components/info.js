73ac39a4eb742b2d293af185538acd2864d5b55a
maksim.k.as@agiliumlabs.com <kulykov.maks@gmail.com>
2022-05-23 16:43:54 +0200

    implemented printing summary letter by letter

--- client/src/components/info.js
+++ client/src/components/info.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import { colors, unit } from '../globalStyles';
 
 const InfoPage = ({ company, ship }) => {
 
+    const [text, setText] = React.useState('');
+
+    const
+        string = company.summary,
+        index = React.useRef(0);
+
+    useEffect(() => {
+        console.log(string);
+        console.log(text);
+        function tick() {
+            setText(text + string[index.current]);
+            index.current++;
+        }
+        if (index.current < string.length) {
+            let addChar = setInterval(tick, 80);
+            return () => clearInterval(addChar);
+        }
+    }, [string, text]);
+
     return (
         <InfoPageContainer>
             <InfoPageImage background_img={ship.image} />
             <InfoPageContent>
-                {company.summary}
+                {text}
             </InfoPageContent>
         </InfoPageContainer>
     );
@@ -33,4 +52,7 @@ const InfoPageImage = styled.div`
 `;
 
 const InfoPageContent = styled.p`
+    margin-top: 30%;
+    font-style: italic;
+    font-size: 32px;
 `;
