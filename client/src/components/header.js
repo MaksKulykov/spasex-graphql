7db92180d3e365cb9fe7bd61312d4f54891dff4f
maksim.k.as@agiliumlabs.com <kulykov.maks@gmail.com>
2022-05-23 15:50:24 +0200

    added logo

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { colors } from '../globalStyles';
+import logo_white from '../assets/logo_white.svg';
 
 /**
  * Header renders the top navigation
@@ -8,7 +9,9 @@ import { colors } from '../globalStyles';
 const Header = () => {
     return (
         <HeaderContainer>
-            <p>This is header</p>
+            <LogoContainer>
+                <Logo src={logo_white} />
+            </LogoContainer>
         </HeaderContainer>
     );
 };
@@ -19,11 +22,20 @@ export default Header;
 const HeaderContainer = styled.div`
     display: flex;
     flex-direction: row;
-    justify-content: center;
+    justify-content: start;
     align-items: center;
     border-bottom: solid 1px ${colors.text};
     box-shadow: 0 1px 5px 0 ${colors.text};
     padding: 5px 30px;
     min-height: 80px;
     background-color: ${colors.background};
+`;
+
+const LogoContainer = styled.div`
+    display: flex;
+    align-self: center;
+`;
+
+const Logo = styled.img`
+    width: 300px;
 `;
